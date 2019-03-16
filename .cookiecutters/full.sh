4023666f897d9fd260490860c96bde8ac6aae9cf
denezt@yahoo.com <denezt@yahoo.com>
2019-03-16 12:31:13 -0500

    DEV-0001: Fixing permission issues and refactoring

---
+++ .cookiecutters/full.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+read -r -d '' doc << EOM
+{% INSERT %}
+EOM
+
+# ---------- Modules ----------
+source gutils.sh
+
+# ---------- Command-line Arguments ----------
+eval set -- "$(getopt -o "d,h,v" -l "debug,help,verbose" -- "$@")"
+
+export USAGE_GRAMMAR=(
+    "[-d] [-v]"
+    "-h"
+)
+
+# shellcheck disable=SC2154
+read -r -d '' help << EOM
+$(usage)
+
+${doc}
+
+Optional Arguments:
+    -d | --debug
+        Enable debug mode.
+
+    -h | --help
+        View this help message.
+
+    -v | --verbose
+        Enable verbose output.
+EOM
+
+while [[ -n "$1" ]]; do
+    case $1 in
+       -d|--debug )
+           debug=true
+           ;;
+       -h|--help )
+           echo "${help}"
+           exit 0
+           ;;
+       -v|--verbose )
+           verbose=true
+           ;;
+       -- )
+           shift
+           break
+           ;;
+    esac
+    shift
+done
+
+if [[ "${debug}" = true && "${verbose}" = true ]]; then
+    PS4='$LINENO: '
+    set -x
+fi
+
+# ---------- Main ----------
---
+++ .cookiecutters/pytest.py
@@ -0,0 +1,10 @@
+{% INSERT %}
+
+import datetime as dt
+import sys  # noqa
+import os  # noqa
+import unittest.mock as mock
+
+import pytest  # noqa
+
+import {{ PY_MODULE }}
