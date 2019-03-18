27af7a823cf0ead1a97e54eaad5a8406e5e263a5
Richard L. Jackson <denezt@yahoo.com>
2019-03-18 21:21:16 +0100

    Adding, cookiecutter placing in setup script.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,12 @@ _files=( 'cookie' )
 apt-get -y update
 apt-get -y install make sudo
 
-
 for f in ${_files[@]}
 do
 	chmod 755 $f
 done
+
+if [ -d ".cookiecutter" ];
+then
+	cp -a -v .cookiecutter /root/.cookiecutter
+fi
